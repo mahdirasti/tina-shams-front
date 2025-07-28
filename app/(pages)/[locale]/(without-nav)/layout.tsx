08445731a5431adcdf8900016aa0426dd61b0881
Your Name <dev.mahdi76@gmail.com>
2025-07-28 23:14:10 +0300

    add locale to paramas

--- app/(pages)/[locale]/(without-nav)/layout.tsx
+++ app/(pages)/[locale]/(without-nav)/layout.tsx
@@ -4,13 +4,17 @@ import LocaleContext from "../locale-context";
 import { LocaleType } from "@/types/locale";
 import { getDictionary } from "../dictionaries";
 
-type Props = LayoutProps & { params: { locale: LocaleType } };
+type Props = LayoutProps & {
+  params: Promise<{ locale: LocaleType }>;
+};
 
 export default async function Layout({ children, params }: Props) {
-  const dict = await getDictionary(params.locale);
+  const { locale } = await params;
+
+  const dict = await getDictionary(locale);
 
   return (
-    <LocaleContext langJson={dict} locale={params.locale}>
+    <LocaleContext langJson={dict} locale={locale}>
       {children}
     </LocaleContext>
   );
